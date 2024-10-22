a5f29671b1dc3b312f94cf815e0403885cecb58a
DoceAzedo <me@doceazedo.com>
2024-10-22 00:24:58 -0300

    Cleanup for building

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+start=`date +%s`
+
+targets=(
+  "aarch64-apple-darwin"
+  "x86_64-pc-windows-gnu"
+)
+
+printf "Setting up...\n"
+rm -rf dist/* 2> /dev/null
+mkdir -p "dist"
+
+printf "Copying project files...\n"
+cp godot/project.godot "dist"
+cp -a godot/addons "dist/addons"
+
+for target in "${targets[@]}"; do
+  printf "Building binaries for $target...\n"
+  (cd rust && cargo build --target=$target --release)
+
+  printf "Copying binaries...\n"
+  mkdir -p "dist/addons/godot_wry/bin/$target"
+  cp -a "rust/target/$target/release/." "dist/addons/godot_wry/bin/$target"
+
+  printf "Built target $target successfully!\n"
+done
+
+printf "Zipping files...\n"
+(cd dist && zip -r "$(date '+%Y-%m-%d').zip" .)
+
+end=`date +%s`
+time_elapsed=$((end-start))
+
+printf "✅ Done! (${time_elapsed}s)\n"
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -13,37 +13,6 @@ struct GodotWRY;
 unsafe impl ExtensionLibrary for GodotWRY {}
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
-struct Player {
-    speed: f64,
-    angular_speed: f64,
-    base: Base<Sprite2D>
-}
-
-#[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
-    }
-
-    fn physics_process(&mut self, delta: f64) {
-        // In GDScript, this would be:
-        // rotation += angular_speed * delta
-
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
-        // The 'rotate' method requires a f32,
-        // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
-    }
-}
-
-#[derive(GodotClass)]
 #[class(base=Control)]
 struct WebView {
     base: Base<Control>,
@@ -77,7 +46,6 @@ struct WebView {
 #[godot_api]
 impl IControl for WebView {
     fn init(base: Base<Control>) -> Self {
-        godot_print!("Hello, webview renderer!");
         Self {
             base,
             webview: None,
