d5dd83d0ef646bd528f276886bc264230d339b94
DoceAzedo <me@doceazedo.com>
2025-03-14 16:09:39 -0300

    Handle local JS files

--- rust/src/protocols.rs
+++ rust/src/protocols.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use godot::classes::{Os, ProjectSettings};
 use http::{Request, Response};
 use http::header::CONTENT_TYPE;
+use infer::{MatcherType, Type};
 
 pub fn get_res_response(
     request: Request<Vec<u8>>,
@@ -21,20 +22,24 @@ pub fn get_res_response(
     let path = format!("{}{}", request.uri().host().unwrap_or_default(), request.uri().path());
     let full_path = root.join(path);
     if full_path.exists() && full_path.is_file() {
-        let content = fs::read(full_path).expect("Failed to read file");
-        let mime = infer::get(&content).expect("File type is unknown");
+        let content = Cow::from(fs::read(&full_path).expect("Failed to read file"));
+        let mut mime = infer::get(&*content)
+            .unwrap_or(Type::new(MatcherType::App, "application/octet-stream", "", |_buf: &[u8]| true))
+            .to_string();
+        if full_path.extension().unwrap_or_default() == "js"  {
+            mime = String::from("text/javascript")
+        }
         return http::Response::builder()
-            .header(CONTENT_TYPE, mime.to_string())
+            .header(CONTENT_TYPE, mime)
             .status(200)
             .body(content)
-            .unwrap()
-            .map(Into::into);
+            .expect("Failed to build 200 response");
     }
 
     http::Response::builder()
         .header(CONTENT_TYPE, "text/plain")
         .status(404)
         .body(format!("Could not find file at {:?}", full_path).as_bytes().to_vec())
-        .unwrap()
+        .expect("Failed to build 404 response")
         .map(Into::into)
 }
