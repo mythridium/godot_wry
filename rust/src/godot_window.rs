a88a7a94f97a80c65bd6e86fe0feaafa8c30c268
Kowalski <darkkowalski2012@gmail.com>
2025-05-30 06:58:48 +0800

    Try to fix for X11 (#33)
    * setup
    
    * fix
    
    * update

--- rust/src/godot_window.rs
+++ rust/src/godot_window.rs
@@ -40,14 +40,52 @@ impl HasWindowHandle for GodotWindow {
 
     #[cfg(target_os = "linux")]
     fn window_handle(&self) -> Result<WindowHandle<'_>, HandleError> {
+        use godot::classes::display_server;
+        use gtk::gdk::prelude::DisplayExtManual;
+        use x11_dl::xlib::{Xlib, CWEventMask, SubstructureNotifyMask, SubstructureRedirectMask, XSetWindowAttributes, XWindowAttributes};
+
         gtk::init().expect("Failed to initialize gtk");
+        if !gtk::gdk::Display::default().unwrap().backend().is_x11() {
+            panic!("GDK backend must be X11");
+        }
+        let xlib = Xlib::open().expect("Failed to open Xlib");
 
         let display_server = DisplayServer::singleton();
-        let window_handle = display_server.window_get_native_handle(HandleType::WINDOW_HANDLE);
+        let window_xid = display_server.window_get_native_handle(HandleType::WINDOW_HANDLE);
+        let display = display_server.window_get_native_handle(HandleType::DISPLAY_HANDLE);
+
+        unsafe {
+            let attributes: XWindowAttributes = std::mem::zeroed();
+            let mut attributes = std::mem::MaybeUninit::new(attributes).assume_init();
+
+            let ok = (xlib.XGetWindowAttributes)(
+                display as _,
+                window_xid as c_ulong,
+                &mut attributes,
+            );
+
+            if ok != 1 {
+                panic!("Failed to get X11 window attributes");
+            }
+
+            let mut set_attributes: XSetWindowAttributes = std::mem::zeroed();
+            set_attributes.event_mask = attributes.all_event_masks & !SubstructureNotifyMask & !SubstructureRedirectMask;
+            let ok = (xlib.XChangeWindowAttributes)(
+                display as _,
+                window_xid as c_ulong,
+                CWEventMask,
+                &mut set_attributes,
+            );
+
+            if ok != 1 {
+                panic!("Failed to change X11 window attributes");
+            }
+        }
+
         unsafe {
             Ok(WindowHandle::borrow_raw(
                 RawWindowHandle::Xlib(XlibWindowHandle::new({
-                    window_handle as c_ulong
+                    window_xid as c_ulong
                 }))
             ))
         }
